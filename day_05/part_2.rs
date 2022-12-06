37844a75d1e44e7215a47ce6ff0fbb540775bf29
sjf25 <sohaibjf@gmail.com>
2022-12-06 03:28:00 -0600

    day 5

---
+++ day_05/part_2.rs
@@ -0,0 +1,45 @@
+use std::io::BufRead;
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut init_stacking: Vec<_> = stdin.lock()
+        .lines()
+        .map(|x| x.unwrap())
+        .take_while(|x| x != "")
+        .collect();
+    init_stacking.pop();
+
+    let moves: Vec<_> = stdin.lock().lines().map(|x| x.unwrap()).collect();
+
+    let num_stacks = (init_stacking[0].len() + 1) / 4;
+    let mut stacks = vec![Vec::<char>::new(); num_stacks];
+    for line in init_stacking.iter().rev() {
+        for i in 0..num_stacks {
+            let sym = line.as_bytes()[i*4 + 1] as char;
+            if sym != ' ' {
+                stacks[i].push(sym);
+            }
+        }
+    }
+
+    for m in moves {
+        let tokens: Vec<_> = m.split_whitespace().collect();
+        let quantity: usize = tokens[1].parse().unwrap();
+        let from: usize = tokens[3].parse().unwrap();
+        let to: usize = tokens[5].parse().unwrap();
+
+        let mut to_move = Vec::new();
+        for _i in 0..quantity {
+            to_move.push(stacks[from-1].pop().unwrap());
+        }
+        while to_move.len() > 0 {
+            stacks[to-1].push(to_move.pop().unwrap());
+        }
+    }
+
+    for s in stacks {
+        print!("{}", s.last().unwrap());
+    }
+    println!();
+
+}
